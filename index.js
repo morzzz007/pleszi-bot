80baaf073c2c4f6cb77d52b477fb1b3585cbc922
Marton Papp <morzzz007@gmail.com>
2016-05-09 20:14:50 +0200

    pleszi random bullshit responses

--- index.js
+++ index.js
@@ -1,248 +1,69 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-
-
-This is a sample Slack bot built with Botkit.
-
-This bot demonstrates many of the core features of Botkit:
-
-* Connect to Slack using the real time API
-* Receive messages based on "spoken" patterns
-* Reply to messages
-* Use the conversation system to ask questions
-* Use the built in storage system to store and retrieve information
-  for a user.
-
-# RUN THE BOT:
-
-  Get a Bot token from Slack:
-
-    -> http://my.slack.com/services/new/bot
-
-  Run your bot from the command line:
-
-    token=<MY TOKEN> node slack_bot.js
-
-# USE THE BOT:
-
-  Find your bot inside Slack to send it a direct message.
-
-  Say: "Hello"
-
-  The bot will reply "Hello!"
-
-  Say: "who are you?"
-
-  The bot will tell you its name, where it running, and for how long.
-
-  Say: "Call me <nickname>"
-
-  Tell the bot your nickname. Now you are friends.
-
-  Say: "who am I?"
-
-  The bot will tell you your nickname, if it knows one for you.
-
-  Say: "shutdown"
-
-  The bot will ask if you are sure, and then shut itself down.
-
-  Make sure to invite your bot into other channels using /invite @<my bot>!
-
-# EXTEND THE BOT:
-
-  Botkit has many features for building cool and useful bots!
-
-  Read all about it here:
-
-    -> http://howdy.ai/botkit
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-
 if (!process.env.token) {
-    console.log('Error: Specify token in environment');
-    process.exit(1);
+  console.log('Error: Specify token in environment');
+  process.exit(1);
 } else {
-    console.log('Running!');
+  console.log('Running!');
 }
 
-var Botkit = require('botkit');
-var os = require('os');
+const Botkit = require('botkit');
+const _ = require('lodash');
 
-var controller = Botkit.slackbot({
-    debug: true,
+const controller = Botkit.slackbot({
+  debug: false,
 });
 
-var bot = controller.spawn({
-    token: process.env.token
+const bot = controller.spawn({
+  token: process.env.token
 }).startRTM();
 
-
-controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
-
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
-    });
-});
-
-controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var name = message.match[1];
-    controller.storage.users.get(message.user, function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
-        }
-        user.name = name;
-        controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-        });
-    });
-});
-
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
-        } else {
-            bot.startConversation(message, function(err, convo) {
-                if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
-                                pattern: 'yes',
-                                callback: function(response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                }
-                            },
-                            {
-                                pattern: 'no',
-                                callback: function(response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                }
-                            },
-                            {
-                                default: true,
-                                callback: function(response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            }
-                        ]);
-
-                        convo.next();
-
-                    }, {'key': 'nickname'}); // store the results in a field called nickname
-
-                    convo.on('end', function(convo) {
-                        if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
-
-                            controller.storage.users.get(message.user, function(err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                });
-                            });
-
-
-
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
-                        }
-                    });
-                }
-            });
-        }
-    });
-});
-
-
-controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.startConversation(message, function(err, convo) {
-
-        convo.ask('Are you sure you want me to shutdown?', [
-            {
-                pattern: bot.utterances.yes,
-                callback: function(response, convo) {
-                    convo.say('Bye!');
-                    convo.next();
-                    setTimeout(function() {
-                        process.exit();
-                    }, 3000);
-                }
-            },
-        {
-            pattern: bot.utterances.no,
-            default: true,
-            callback: function(response, convo) {
-                convo.say('*Phew!*');
-                convo.next();
-            }
-        }
-        ]);
-    });
-});
-
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-    'direct_message,direct_mention,mention', function(bot, message) {
-
-        var hostname = os.hostname();
-        var uptime = formatUptime(process.uptime());
-
-        bot.reply(message,
-            ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
-    });
-
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
-
-    uptime = uptime + ' ' + unit;
-    return uptime;
+const names = {
+  'U0WRVC9DY': 'marci',
+  'U0WU38WGP': 'karcsika',
+  'U0WTSCMMX': 'attila',
+  'U0WSEJZU5': 'adri',
+  'U0WT9MWQ4': 'dubo',
+  'U0WGR1C5Q': 'buzi',
+  'U0WTADL3W': 'nandi',
 }
+
+const bullshit = [
+  'még serkenő bajszú piarista voltam, amikor először lépett az életembe a hiperszuper git tag feature',
+  'már akkor éreztem, hogy ez a találkozás nagyobb lesz, mint Kóka János önbizalma',
+  'már a hétvégére gondolva elkezdtem etetőanyagot gyűjteni a Virtuoso bugfix event alkalmából összegyűlt tömegben',
+  'mindig élmény látni, ahogy Kovács László nevére melegíteni kezdenek a főállású tüntetők, vagy mint amikor Budai Gyula piros tooltipről akarja megtanulni a virtuoso irányítását',
+  'a Virtuoso backend updatelés a szekértáborhívők Sziget fesztiválja, az sem lenne meglepőbb, ha Horn Gábor Áder Jánossá operáltatná magát nyaktól fölfelé',
+  'angular app fejlesztésére javascript developert keres a Lensa',
+  'a pozíció egy másfél éves múltú Angular alkalmazas frontend fejlesztéséről szól',
+  'második frontendesként csatlakoznál, egy 3 backendes, egy dedikált tesztelő és egy frontendesből álló csapathoz',
+  'a fejlesztés ES6-ban történik, és a saját kódod teszttelését is magában foglalja',
+  'ES6', 'AngularJS', 'React', 'babel', 'webpack', 'gulp',
+  'fejlesztőbarát szemlélet, cél a minőségi fejlesztés',
+  'a Lensa egy 15 éve a piacon levő cég, többek között a piacvezető karrier portál, a Profession.hu megalkotója',
+  'budapesti irodánkban 30-an dolgozunk, ebből 21 fejlesztő.',
+  'szerintem a backenden lesz valami, vagy nem tudom'
+]
+
+const epam = [
+  'epam? jövőhéten ott kezdek!',
+  'az epamban csak legjobbak dolgoznak, én is jelentkeztem',
+]
+
+controller.hears(['hello', 'hi', '^re$'], ['message_received', 'direct_message', 'direct_mention', 'ambient'], function(bot, message) {
+  if (names[message.user] && message.user != names['U0WGR1C5Q']) {
+    bot.reply(message, `csá ${names[message.user]}`);
+  } else if (message.user != names['U0WGR1C5Q']) {
+    bot.reply(message, `helló főnök, örülök, hogy lassan veled dolgozhatok!`); 
+  }
+});
+
+controller.hears(['epam'], ['message_received', 'direct_message', 'direct_mention', 'ambient'], function(bot, message) {
+  bot.reply(message, _.sample(epam)); 
+});
+
+controller.hears(['pleszi', 'plészi'], ['message_received', 'direct_message', 'direct_mention', 'ambient'], function(bot, message) {
+  bot.reply(message, _.sample(bullshit)); 
+});
+
+controller.on('message_received', function(bot, message) {
+  console.log(message.user, message);
+});
